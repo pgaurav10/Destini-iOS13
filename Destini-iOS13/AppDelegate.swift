bcebf4febfde870ba526ee5c7681430f957fd84b
Gaurav Patil <gaurav.patil.atwork@gmail.com>
2024-01-31 21:37:54 -0800

    Update AppDelegate.swift

--- Destini-iOS13/AppDelegate.swift
+++ Destini-iOS13/AppDelegate.swift
@@ -2,8 +2,7 @@
 //  AppDelegate.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Gaurav Patil.
 //
 
 import UIKit
