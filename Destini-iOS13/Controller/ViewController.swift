8c8f2895e16819fd1b517ca0e9065d76b59ea860
Gaurav <gauravpatil24121997@gmail.com>
2024-01-31 17:50:02 -0800

    Add existing project files to Git

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
+        
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.stories[storyBrain.storyNo].title
+        
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNo].choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNo].choice2, for: UIControl.State.normal)
+    }
 }
 
